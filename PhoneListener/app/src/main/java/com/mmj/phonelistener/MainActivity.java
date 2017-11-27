b81c65205f55646bacab22ca6425cc444baf6d41
Kos <1624912549@qq.com>
2017-11-28 05:06:33 +0800

    finish phonelistener

--- PhoneListener/app/src/main/java/com/mmj/phonelistener/MainActivity.java
+++ PhoneListener/app/src/main/java/com/mmj/phonelistener/MainActivity.java
@@ -3,17 +3,22 @@ package com.mmj.phonelistener;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    private TextView textView,textView1;
+    private String incomingNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=(TextView)findViewById(R.id.TeleInfo);
-
+        textView1=(TextView)findViewById(R.id.call);
         //Get the instance of TelephonyManager
         TelephonyManager tm=(TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
 
@@ -45,5 +50,26 @@ public class MainActivity extends AppCompatActivity {
         info+="\n In Roaming? :"+isRoaming;
         textView.setText(info);//displaying the information in the textView
 
+        PhoneStateListener pListener=new PhoneStateListener(){ /*注册一个监听器*/
+            @Override
+            public void onCallStateChanged(int state, String number) {
+                switch (state) {
+                    case TelephonyManager.CALL_STATE_IDLE://无任何状态
+                        break;
+                    case TelephonyManager.CALL_STATE_OFFHOOK://接听来电
+                        textView1.setText("接听了"+number+"的来电\n");
+                        Toast.makeText(getApplicationContext(),number,Toast.LENGTH_LONG).show();
+                        break;
+                    case TelephonyManager.CALL_STATE_RINGING://来电
+                        textView1.setText(number+"来电\n");
+                        Toast.makeText(getApplicationContext(),number,Toast.LENGTH_LONG).show();
+                        break;
+                    default:
+                        break;
+                }
+                super.onCallStateChanged(state, incomingNumber);
+            }
+        };
+        tm.listen(pListener,PhoneStateListener.LISTEN_CALL_STATE);
     }
 }
